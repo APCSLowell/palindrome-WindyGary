f0921aa5777206d118bcd7a9e42f51c4cd919d9e
WindyGary <116767332+WindyGary@users.noreply.github.com>
2024-01-17 08:54:15 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -44,6 +44,9 @@ public String reverse(String str)
 {
     String sNew = new String();
     //your code here
+    for (int i = str.length()-1; i >= 0; i--){
+      sNew += str.charAt(i);
+    }
     return sNew;
 }
 }
